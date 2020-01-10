2348c024bff43bf8b5ea3602acb01f787cb93500
Erhun Giray TUNCAY <48091473+giraygi@users.noreply.github.com>
2020-01-11 01:55:23 +0300

    p-value calculation of multiple cluster pairs

---
+++ RKIDiseaseCluster/ClusterSignificance.R
@@ -0,0 +1,27 @@
+
+library(sigclust)
+fullpath <- "/home/giray/workspace/RKIDiseaseCluster/rki_data/mutatesfull/"
+mutations <- list.files(fullpath,"mutations_louvain2")
+drugresistances <- list.files(fullpath,"drugresistances_louvain2")
+nsim <- 2000
+nrep <- 1
+icovest <- 1
+
+# for (i in 1:(length(mutations)/2)) {
+#   assign(paste("pvalue",mutations[(i*2)-1],sep='_'),sigclust(read.table(paste(fullpath,mutations[(i*2)],sep=""),header=TRUE,sep=" "),nsim=nsim,nrep=nrep,labflag=1,label=read.table(paste(fullpath,mutations[(i*2)-1],sep=""),header=FALSE,sep=" "),icovest=icovest))
+# }
+
+for (i in 1:(length(mutations)/2)) {
+  dat <- read.table(paste(fullpath,mutations[(i*2)],sep=""),header=TRUE,sep=" ")
+  lab  <- read.table(paste(fullpath,mutations[(i*2)-1],sep=""),header=FALSE,sep=" ")
+  assign(strsplit(paste("pvalue",mutations[(i*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest))
+}
+icovest <- 3
+for (j in 1:(length(drugresistances)/2)) {
+  dat <- read.table(paste(fullpath,drugresistances[(j*2)],sep=""),header=TRUE,sep=" ")
+  lab  <- read.table(paste(fullpath,drugresistances[(j*2)-1],sep=""),header=FALSE,sep=" ")
+  assign(strsplit(paste("pvalue",drugresistances[(j*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest))
+}
+
+
+# strsplit(paste("pvalue",mutations[1],sep='_'),"_labels.txt")[[1]][1]
