854332c09dd32410273ccff96a9ced792c6ef8e5
Erhun Giray TUNCAY <48091473+giraygi@users.noreply.github.com>
2020-01-16 15:02:14 +0300

    exception handling mechanism of Cluster Significance replaced with try-if

--- RKIDiseaseCluster/ClusterSignificance.R
+++ RKIDiseaseCluster/ClusterSignificance.R
@@ -1,6 +1,5 @@
-
 library(sigclust)
-fullpath <- "/home/giray/workspace/RKIDiseaseCluster/rki_data_35140/mutates01-05/"
+fullpath <- "/media/giray/Windows/Nextcloud/transmission networks/data/rki_data_35140/transmits13-05/"
 mutations <- list.files(fullpath,"mutations_")
 drugresistances <- list.files(fullpath,"drugresistances_")
 nsim <- 2000
@@ -15,28 +14,37 @@ for (i in 1:(length(mutations)/2)) {
   dat <- read.table(paste(fullpath,mutations[(i*2)],sep=""),header=TRUE,sep=" ")
   lab  <- read.table(paste(fullpath,mutations[(i*2)-1],sep=""),header=FALSE,sep=" ")
   icovest <- 1
-  tryCatch(
-    {
-      assign(strsplit(paste("pvalue",mutations[(i*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest))     
-    },
-    error=function(e) {
-      icovest <-2
-      assign(strsplit(paste("pvalue",mutations[(i*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest))
-    })
+errorstatus<-try(assign(strsplit(paste("pvalue",mutations[(i*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest))     )
+      if(class(errorstatus)=="try-error")
+      {
+        icovest <-2
+        errorstatus2<-try(assign(strsplit(paste("pvalue",mutations[(i*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest)))
+        if(class(errorstatus2)=="try-error"){
+          icovest <-3
+          errorstatus3<-try(assign(strsplit(paste("pvalue",mutations[(i*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest)))
+          if(class(errorstatus3)=="try-error"){
+            assign(strsplit(paste("pvalue",mutations[(i*2)-1],sep='_'),"_labels.txt")[[1]][1],i)
+            next}
+        }
+      }
 }
 icovest <- 1
 for (j in 1:(length(drugresistances)/2)) {
   dat <- read.table(paste(fullpath,drugresistances[(j*2)],sep=""),header=TRUE,sep=" ")
   lab  <- read.table(paste(fullpath,drugresistances[(j*2)-1],sep=""),header=FALSE,sep=" ")
   icovest <- 1
-  tryCatch(
-    {
-      assign(strsplit(paste("pvalue",drugresistances[(j*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest))
-    },
-    error=function(e) {
-      icovest <-2
-      assign(strsplit(paste("pvalue",drugresistances[(j*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest))
-    })
+  errorstatus<-try(assign(strsplit(paste("pvalue",drugresistances[(j*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest)))
+  if(class(errorstatus)=="try-error"){
+    icovest <-2
+    errorstatus2<-try(assign(strsplit(paste("pvalue",drugresistances[(j*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest)))
+    if(class(errorstatus2)=="try-error"){
+      icovest <-3
+      errorstatus3<-try(assign(strsplit(paste("pvalue",drugresistances[(j*2)-1],sep='_'),"_labels.txt")[[1]][1],sigclust(dat,nsim=nsim,nrep=nrep,labflag=1,label=lab,icovest=icovest)))
+      if(class(errorstatus3)=="try-error"){
+        assign(strsplit(paste("pvalue",drugresistances[(j*2)-1],sep='_'),"_labels.txt")[[1]][1],j)
+        next}
+    }
+  }
 }
 
 
